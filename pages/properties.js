516b46055695c5c35c83a70d7f8df5a1eb7e3a03
Jorell Martis <jorell@tentwenty.me>
2022-11-20 19:19:30 +0530

    Initiazlized useEffect for selected options

--- pages/properties.js
+++ pages/properties.js
@@ -5,9 +5,17 @@ import PropertyList from '../components/blocks/PropertyList'
 
 const properties = ({filterData}) => {
     const[properties , setProperties] = useState(null);
+    const [selectedLocation, setSelectedLocation] = useState(null);
+    const [selectedNumberOfRunways, setSelectedNumberOfRunways] = useState(null);
     useEffect(() => {
         getProperites();
     }, []);
+    useEffect(() => {
+        console.log(selectedLocation)
+    }, [selectedLocation]);
+    useEffect(() => {
+        console.log("use effect of runways")
+    }, [selectedNumberOfRunways]);
     const getProperites = async () => {
     try {
         const { data } = await apolloClient.query({
@@ -21,10 +29,28 @@ const properties = ({filterData}) => {
     return (
         <>
         <div>
-            <select name="location">
+            <select name="location"
+                defaultValue={0}
+                onChange={(e) => setSelectedLocation(e.target.value)}
+            >
+                <option disabled value={0}>
+                Select Location
+                </option>
                 {filterData?.listLocations?.data.map((location) => (
-                    <option key={`location-dropdown-${location?.id}`} value={location?.id} >
-                        {location.attributes.title}
+                    <option key={`location-dropdown-${location?.id}`} value={location?.attributes?.title} >
+                        {location?.attributes?.title}
+                    </option>
+                ))}
+            </select>
+            <select name="runways"
+                defaultValue={0}
+            >
+                <option disabled value={0}>
+                    Number Of Runways
+                </option>
+                {filterData?.listRunways?.data.map((runway) => (
+                    <option key={`runway-dropdown-${runway?.id}`} value={runway?.id} >
+                        {runway.attributes.title}
                     </option>
                 ))}
             </select>
